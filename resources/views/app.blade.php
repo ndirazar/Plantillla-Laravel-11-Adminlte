6cdf3003ced087de3124c8213c2f846431872bf8
Nicolas Dirazar~ <jndirazar@gmail.com>
2024-09-24 18:15:34 -0300

    Inicio

---
+++ resources/views/app.blade.php
@@ -0,0 +1,46 @@
+@extends('adminlte::page')
+
+@section('title', 'Admin Dashboard')
+
+@section('content_header')
+    <h1>Admin Dashboard</h1>
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item active">Dashboard</li>
+    </ol>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>150</h3>
+                    <p>New Orders</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>53<sup style="font-size: 20px">%</sup></h3>
+                    <p>Bounce Rate</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <!-- Add more boxes as needed -->
+    </div>
+    <p>Welcome to this beautiful admin panel. Here you can manage all your data efficiently.</p>
+@endsection
